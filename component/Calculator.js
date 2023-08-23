5ff151048b866ea90f4a5cf49f8394d8e2c406d6
kiran-c8 <kiranc8298@gmail.com>
2023-08-24 00:07:07 +0530

    updated code

--- component/Calculator.js
+++ component/Calculator.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const Calculator = () => {
-
-  let [num, setNum] = useState('0');
+  let [num, setNum] = useState(0);
   let [res, setRes] = useState(0);
   let [sign, setSign] = useState("");
 
   // Function which handles number inputs
   const numClickHandler = (value) => {
-    if (num === "0" && (value === "00" || value === "0")) {
+    if (num === 0 && (value === "00" || value === "0")) {
       setNum("0");
     } else {
-      setNum(parseFloat(num + value));
-      
+      setNum(parseFloat(num.toString() + value));
     }
   };
 
@@ -29,7 +27,11 @@ const Calculator = () => {
     if (sign === "+") {
       setNum(num + res);
     } else if (sign === "/") {
-      setNum(res / num);
+      if (num === 0) {
+        setNum("Error");
+      } else {
+        setNum(res / num);
+      }
     } else if (sign === "-") {
       setNum(res - num);
     } else if (sign === "x") {
@@ -61,90 +63,152 @@ const Calculator = () => {
   };
 
   const backspaceHandler = () => {
-    if(num=='0' || num==''){
+    if (num === 0 || num === "") {
       return;
-    }
-    if (num.length == 1) {
-      setNum('0');
+    } else if (
+      num.toString().length === 1 ||
+      num === "Error" ||
+      (num.toString().length === 2 && num.toString().charAt(0) === "-")
+    ) {
+      setNum(0);
     } else {
       const newNum = parseFloat(num.toString().slice(0, -1));
-      setNum(newNum.toString());
+      setNum(newNum);
     }
   };
 
-
   return (
     <View style={styles.container}>
-      <Text style={styles.display}>{num.toString().length >= 16
-              ? parseFloat(num).toExponential(8)
-              : num}</Text>
+      <Text style={styles.display}>
+        {num.toString().length >= 16 ? parseFloat(num).toExponential(8) : num}
+      </Text>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.button} onPress={() => backspaceHandler()}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => backspaceHandler()}
+        >
           <Text style={styles.buttonText}>C</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => invertClickHandler()}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => invertClickHandler()}
+        >
           <Text style={styles.buttonText}>+/-</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => signClickHandler('%')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => signClickHandler("%")}
+        >
           <Text style={styles.buttonText}>%</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton} onPress={() => signClickHandler('/')}>
+        <TouchableOpacity
+          style={styles.operatorButton}
+          onPress={() => signClickHandler("/")}
+        >
           <Text style={styles.buttonText}>/</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('7')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("7")}
+        >
           <Text style={styles.buttonText}>7</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('8')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("8")}
+        >
           <Text style={styles.buttonText}>8</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('9')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("9")}
+        >
           <Text style={styles.buttonText}>9</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton} onPress={() => signClickHandler('+')}>
+        <TouchableOpacity
+          style={styles.operatorButton}
+          onPress={() => signClickHandler("+")}
+        >
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('4')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("4")}
+        >
           <Text style={styles.buttonText}>4</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('5')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("5")}
+        >
           <Text style={styles.buttonText}>5</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('6')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("6")}
+        >
           <Text style={styles.buttonText}>6</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton} onPress={() => signClickHandler('-')}>
+        <TouchableOpacity
+          style={styles.operatorButton}
+          onPress={() => signClickHandler("-")}
+        >
           <Text style={styles.buttonText}>-</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('1')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("1")}
+        >
           <Text style={styles.buttonText}>1</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('2')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("2")}
+        >
           <Text style={styles.buttonText}>2</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('3')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("3")}
+        >
           <Text style={styles.buttonText}>3</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton} onPress={() => signClickHandler('x')}>
+        <TouchableOpacity
+          style={styles.operatorButton}
+          onPress={() => signClickHandler("x")}
+        >
           <Text style={styles.buttonText}>*</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.button} onPress={() => resetClickHandler()}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => resetClickHandler()}
+        >
           <Text style={styles.buttonText}>AC</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => numClickHandler('0')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => numClickHandler("0")}
+        >
           <Text style={styles.buttonText}>0</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => commaHandler('.')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => commaHandler(".")}
+        >
           <Text style={styles.buttonText}>.</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton} onPress={() => equalsClickHandler()}>
+        <TouchableOpacity
+          style={styles.operatorButton}
+          onPress={() => equalsClickHandler()}
+        >
           <Text style={styles.buttonText}>=</Text>
         </TouchableOpacity>
       </View>
@@ -156,34 +220,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     // alignItems:"flex-end",
-    backgroundColor: '#F5F5F5',
+    backgroundColor: "#F5F5F5",
   },
   display: {
     fontSize: 48,
-    textAlign: 'right',
+    textAlign: "right",
     marginBottom: 20,
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 3,
   },
   button: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#D3D3D3',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#D3D3D3",
     padding: 23,
     borderRadius: 10,
-    margin:3
+    margin: 3,
   },
   operatorButton: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFA500',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFA500",
     padding: 20,
     borderRadius: 10,
   },
